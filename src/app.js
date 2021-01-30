3dac83670ee70665c6936af1e4abc405b9ae1ce0
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-30 13:37:54 +0300

    impoted the formsidebarcontainer

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import JobListContainer from "./container/job-list"
 import FormContainer from "./container/form"
+import FormSidebarContainer from "./container/form-sidebar"
 
 export default function App() {
     return (
         <div>
             <h1>Github jobs</h1>
             <FormContainer/>
+            <FormSidebarContainer/>
             <JobListContainer />
         </div>
     )
