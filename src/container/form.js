0cf5d93fa5b3a12d398b56e90aa255dd0108ce9e
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-30 11:09:13 +0300

    createc the formContainer

---
+++ src/container/form.js
@@ -0,0 +1,28 @@
+import React, { useContext, useState } from 'react'
+import { Context } from "../context/Context"
+export default function FormContainer() {
+    const {state, dispatch} = useContext(Context)
+    const {jobs} = state
+    const [searchForJob, setSearchForJob] = useState("")
+
+    function searchForJobInput(e) {
+        e.preventDefault()
+        const filteredJobByName = jobs.filter(job => job.title.toLowerCase().includes(searchForJob))
+        dispatch({type: "SEARCH_JOB_BY_NAME", value: filteredJobByName})
+        setSearchForJob("")
+    }
+    return (
+        <div className="search-jobs">
+            <form className="form-input" onSubmit={searchForJobInput}>
+                <i className="ri-earth-line"></i>
+                <input
+                type="text"
+                placeholder="Type the title of the job"
+                value={searchForJob}
+                onChange={(e) => setSearchForJob(e.target.value)}
+                />
+                <button className="search-button">Search</button>
+            </form>
+        </div>
+    )
+}
