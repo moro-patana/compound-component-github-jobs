cfa63870b7703d58ee18db3e65a2a7583795a117
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-30 14:58:56 +0300

    created the jobdetails

---
+++ src/container/job-details.js
@@ -0,0 +1,44 @@
+import React, { useContext } from "react"
+import { Context } from "../context/Context"
+import { Link, useParams } from "react-router-dom"
+function JobDetails() {
+    const { state, dispatch } = useContext(Context)
+    const { jobs } = state
+    const { jobId } = useParams()
+    const job = jobs.find(job => job.id === jobId)
+
+    return (
+        <div className="job-description">
+            <div>
+                <Link className="back-to-search" to="/">⬅Back to search</Link>
+                <h2>How to apply</h2>
+                <div className="address">
+                    <p>Please email a copy of your resume and online portfolio to <a href="/wes">wes@kasisto.com</a> & CC <a href="/eric">eric@kasisto.com</a></p>
+                </div>
+            </div>
+            <article key={job?.id} className="jobs-details">
+                <div className="details-heading">
+                    <p className="title">{job?.title}</p>
+                    <button className="type">{job?.type}</button>
+                </div>
+                <br></br>
+                <span className="date">{new Date(job?.created_at).toLocaleDateString()}</span>
+                <div className="company">
+                    <img className="company-logo" src={job?.company_logo} alt={job?.company} />
+                    <div className="company-info">
+                        <p className="company-name">{job?.company}</p>
+                        <span className="location">
+                            <i className="ri-earth-line"></i>
+                            {job?.location}
+                        </span>
+                    </div>
+                </div>
+                <div className="description" dangerouslySetInnerHTML={{
+                    __html: job?.description
+                }}></div>
+            </article>
+        </div>
+
+    )
+}
+export default JobDetails;
