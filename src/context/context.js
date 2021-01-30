8c35db0066ff1bf8abc99c8f7a58b9132cd10eab
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-30 09:07:18 +0300

    fetched the data

--- src/context/context.js
+++ src/context/context.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useReducer, useEffect} from 'react'
 const Context = React.createContext()
+const JOBS_API = " https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description"
 export default function ContextProvider({children}) {
+    const [state, dispatch] = useReducer((state, action) => {
+        switch (action.type) {
+            case "FETCH_DATA": {
+                return {
+                    ...state, loading: false, jobs: action.jobs
+
+                }
+            }
+        }
+    }, {
+        jobs: [],
+        loading: true
+    })
+    useEffect(() => {
+        fetch(JOBS_API)
+            .then(response => response.json())
+            .then(data => dispatch({ type: "FETCH_DATA", jobs: data }))
+    }, []);
     return (
-        <Context.Provider value={{}}>
+        <Context.Provider value={{state, dispatch}}>
             {children}
         </Context.Provider>
     )
