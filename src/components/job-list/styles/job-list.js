90700d56cc1d326437e127d4b2d28db1600035ef
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-30 11:02:44 +0300

    added more styles to the job list

---
+++ src/components/job-list/styles/job-list.js
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+
+export const Container = styled.article`
+    padding: 1rem;
+    display: grid;
+    grid-template-columns: 20% 70%;
+    column-gap: 30px;
+    background: #fff;
+    box-shadow: 0 2px 4px rgb(0 0 0 / 5%);
+    border-radius: 4px;
+    margin-bottom: 30px;
+`;
+export const Title = styled.h2`
+font-size: 20px;
+`;
+export const SubTitle = styled.h3`
+     margin: 0;
+    font-size: 20px;
+`;
+export const Button = styled.button`
+ border: 1px solid black;
+ padding: 1rem;
+ background-color: transparent;
+`;
+export const Location = styled.p``;
+export const Image = styled.img`
+width: 100%;
+`;
+export const Date = styled.p``;
