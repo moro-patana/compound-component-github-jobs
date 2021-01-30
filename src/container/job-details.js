be316657e5339701d0ef2d2096c522f0840a5985
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-30 15:01:56 +0300

    added cases

--- src/container/job-details.js
+++ src/container/job-details.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react"
-import { Context } from "../context/Context"
+import { Context } from "../context/context"
 import { Link, useParams } from "react-router-dom"
 function JobDetails() {
     const { state, dispatch } = useContext(Context)
