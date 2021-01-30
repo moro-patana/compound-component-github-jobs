b122bd57c5dad408c1c59afab85d3c5985dce68f
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-30 09:37:33 +0300

    imported jobList

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import JobList from "./container/job-list"
 
 export default function App() {
     return (
         <div>
             <h1>Github jobs</h1>
+            <JobList/>
         </div>
     )
 }
