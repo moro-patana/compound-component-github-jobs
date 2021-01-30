413c24d3c9f37be13ad76935acb0e67ebcc7809c
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-30 11:01:05 +0300

    imported globalStyles

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom"
 import {BrowserRouter as Router} from "react-router-dom"
 import ContextProvider from "./context/context"
 import App from "./app"
+import { GlobalStyles } from "./global-styles"
 
 
 ReactDOM.render(
     <ContextProvider>
          <Router>
+             <GlobalStyles/>
             <App/>
         </Router>
     </ContextProvider>, document.getElementById('root'))
