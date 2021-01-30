5fff4680725075a16ca5ca7280cec02bdba16d6a
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-30 11:02:17 +0300

    imported all the Styles

---
+++ src/components/job-list/index.js
@@ -0,0 +1,36 @@
+import React from "react"
+import {
+    Container,
+    Title,
+    SubTitle,
+    Button,
+    Location,
+    Image, 
+    Date
+} from "./styles/job-list"
+
+export default function JobList({children, ...restProps}) {
+    return (
+        <Container {...restProps}>
+            {children}
+        </Container>
+    )
+}
+JobList.Title = function JobListTitle({ children, ...restProps }) {
+	return <Title {...restProps}>{children}</Title>;
+};
+JobList.SubTitle = function JobListSubTitle({ children, ...restProps }) {
+	return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+JobList.Button = function JobListButton({ children, ...restProps }) {
+	return <Button {...restProps}>{children}</Button>;
+};
+JobList.Location = function JobListLocation({ children, ...restProps }) {
+	return <Location {...restProps}>{children}</Location>;
+};
+JobList.Image = function JobListImage({...restProps }) {
+	return <Image {...restProps}/>;
+};
+JobList.Date = function JobListDate({ children, ...restProps }) {
+	return <Date {...restProps}>{children}</Date>;
+};
