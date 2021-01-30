d7d9b7e13a80b30c498cfc847d11587ef513dbc8
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-30 11:18:49 +0300

    imported the Input, container and the button

---
+++ src/components/form/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Input, Button} from "./styles/form"
+export default function Form({children, ...restProps}) {
+    return (
+        <Container {...restProps}>
+            {children}
+        </Container>
+    )
+}
+Form.Input = function FormInput({children, ...restProps}) {
+    return <Input {...restProps}></Input>
+}
+Form.Button = function FormButton({children, ...restProps}) {
+    return <Button {...restProps}></Button>
+}
+
