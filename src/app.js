db454856f247f4df5e2e9e982191c30f1d7f9104
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-30 11:13:03 +0300

    imported the FormContainer

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import JobListContainer from "./container/job-list"
+import FormContainer from "./container/form"
 
 export default function App() {
     return (
         <div>
             <h1>Github jobs</h1>
+            <FormContainer/>
             <JobListContainer />
         </div>
     )
