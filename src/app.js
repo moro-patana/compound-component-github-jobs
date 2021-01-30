8bf59b36ab0b5b754e96500b140661e65218abc6
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-01-30 10:36:00 +0300

    changed the joblist into joblistcontainer

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import JobList from "./container/job-list"
+import JobListContainer from "./container/job-list"
 
 export default function App() {
     return (
         <div>
             <h1>Github jobs</h1>
-            <JobList/>
+            <JobListContainer />
         </div>
     )
 }
